676533abeb67852854148faab1be4d444da1b9d0
hmr218 <hmr218@lehigh.edu>
2014-08-29 14:35:48 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//Holland Roza
+//Demo 0
+//Welcome Class Java Program
+//
+
+public class WelcomeClass{
+
+  public static void main(String[] args) {  
+
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-H--M--R--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("My name is Holland Roza and I have three sisters and a dog.");
+  }
+}
+    
+    
+    
+    
+    
