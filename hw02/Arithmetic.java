91355dca2559ad2f287f83521252727c6a9e681e
hmr218 <hmr218@lehigh.edu>
2014-09-08 02:27:00 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -28,16 +28,19 @@ public class Arithmetic{
         totalSockCost$=(nSocks * sockCost$);
         totalSalesTaxS$ =(totalSockCost$ * taxPercent);
         totalSockTax$=(totalSockCost$) + (totalSalesTaxS$);
+        double totalAfterTaxS$=totalSalesTaxS$+totalSockCost$;
         
         double totalGlassCost$, totalGlassTax$, totalSalesTaxG$;
         totalGlassCost$=(nGlasses * glassCost$);
         totalSalesTaxG$=(totalGlassCost$ * taxPercent);
         totalGlassTax$=(totalGlassCost$) + (totalSalesTaxG$);
+        double totalAfterTaxG$=totalSalesTaxG$+totalGlassCost$;
         
         double totalEnvelopeCost$, totalEnvelopeTax$, totalSalesTaxE$;
         totalEnvelopeCost$=(nEnvelopes * envelopeCost$);
         totalSalesTaxE$=(totalEnvelopeCost$ * taxPercent);
         totalEnvelopeTax$=(totalEnvelopeCost$) + (totalSalesTaxE$);
+        double totalAfterTaxE$=totalSalesTaxE$+totalEnvelopeCost$;
         
         double totalCost$=(totalSockCost$ + totalGlassCost$ + totalEnvelopeCost$);
         double totalCostTax$=(totalSockTax$ + totalGlassTax$ + totalEnvelopeTax$);
@@ -47,20 +50,22 @@ public class Arithmetic{
         System.out.println("The socks cost " +  sockCost$ + " per pair.");
         System.out.println("The total cost of the socks before tax is " + totalSockCost$ + ".");
         System.out.println("The total sales tax of the socks is " + totalSalesTaxS$ + ".");
+        System.out.println("The total cost of the socks after tax is " + totalAfterTaxS$ + ".");
         
         System.out.println("I am buying " + nGlasses + " glasses.");
         System.out.println("The glasses cost " + glassCost$ + " per glass.");
         System.out.println("The total cost of the glasses before tax is " + totalGlassCost$ + ".");
         System.out.println("The total sales tax of the glasses is " + totalSalesTaxG$ + ".");
+        System.out.println("The total cost of the glasses after tax is " + totalAfterTaxG$ + ".");
         
         System.out.println("I am buying " + nEnvelopes + " envelope.");
-        System.out.println("The envelopes cost " + envelopeCost$ + " per envelope.");
-        System.out.println("The total cost of the envelopes before tax is " + totalEnvelopeCost$ + ".");
-        System.out.println("The total sales tax of the envelopes is " + totalSalesTaxE$ + ".");
+        System.out.println("The envelope cost " + envelopeCost$ + " per envelope.");
+        System.out.println("The total cost of the envelope before tax is " + totalEnvelopeCost$ + ".");
+        System.out.println("The total sales tax of the envelope is " + totalSalesTaxE$ + ".");
+        System.out.println("The total cost of the envelope after tax is " + totalAfterTaxE$ + ".");
         
         System.out.println("The total cost of the purchases before tax is " + totalCost$ + ".");
         System.out.println("The total sales tax is " + totalSalesTax$ + ".");
         System.out.println("The total cost of the purchases after tax is " + totalCostTax$ + ".");
-        
     } 
 }
